10778aae08dc9c14a15090622220191e19068524
kusaka <rakugaki.oukoku@gmail.com>
2016-05-20 04:04:17 +0900

    db:seed実行用サンプル・データを作成

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+# seedsのサンプル
+Tag.create(naem: 'tag_003', tagcode: 'tag_0003')
